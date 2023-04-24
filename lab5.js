5bf3ec0643d3fece5cd6635fc43013f7f177d43b
John Luis Magtoto <jfmagtoto@up.edu.ph>
2023-04-24 12:39:26 +0800

    extra: lab5_1.js

--- lab5.js
+++ lab5.js
@@ -5,15 +5,12 @@ function time_now() {
     var date = months[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear() + ', ' + days[today.getDay()];
     var time = today.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
-    document.write("Today is " + date);
-    document.write("<br>");
-    document.write("The current time is " + time);
+    document.getElementById("current-date-time").innerHTML = "Today is " + date + "<br>" + "The current time is " + time;
 }
 
 time_now();
 
-// Define a Student class
-function Student(name, age, email, course) {
+function Student (name, age, email, course) {
     this.Student_ID = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
     this.name = name;
     this.age = age;
@@ -21,49 +18,61 @@ function Student(name, age, email, course) {
     this.course = course;
 }
 
-// Create an empty array to hold the students
 let students = [];
 
-// Define the add_student function
 function add_student() {
-    // Get the form values
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let email = document.getElementById("email").value;
     let course = document.querySelector('input[name="course"]:checked').value;
 
-    // Create a new Student object
+    // required fields
+    if (name.trim() === "" || age.trim() === "" || email.trim() === "") {
+        alert("Please fill in all required fields.");
+        return;
+    }
+    
+    // email restrictions
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    // age restrictions
+    if (age === "" || age <= 0 || age > 100) {
+        alert("Please enter a valid age between 1 and 100.");
+        return;
+    }
+
     let student = new Student(name, age, email, course);
 
-    // Add the student to the array
     students.push(student);
 
-    // Clear the form
     document.getElementById("student-form").reset();
 
-    // Optionally, display a message to the user
     alert("Student added! Student ID: " + student.Student_ID);
 }
+console.log(students);
 
-
-  
 function findStudent() {
+    // get data from user input
     const searchId = (document.getElementById("searchId").value);
-  
-    const student = students.find(s => s.Student_ID === searchId);
-  
+    
+    // find student with matching ID
+    const student = students.find(function(s) {return s.Student_ID === searchId;});
+
+    // display student info
     if (student) {
-      document.getElementById("display-list").innerHTML = `<br>Name: ${student.name}<br>Age: ${student.age}<br>Course: ${student.course}`;
+        document.getElementById("find-student").innerHTML = `Name: ${student.name}<br>Age: ${student.age}<br>Course: ${student.course}`;
     } else {
-      alert("Unable to find student with that ID.");
+        alert("Unable to find the student with that ID.");
     }
-  }
+}
 
 function display_list() {
     let display = "";
     for (let i = 0; i < students.length; i++) {
-      display += `<br>Student_ID: ${students[i].Student_ID}<br>Name: ${students[i].name}<br>Age: ${students[i].age}<br>Email: ${students[i].email}<br>Course: ${students[i].course}<br>`;
+        display += `Student_ID: ${students[i].Student_ID}<br>Name: ${students[i].name}<br>Age: ${students[i].age}<br>Email: ${students[i].email}<br>Course: ${students[i].course}<br><br>`;
     }
     document.getElementById("display-list").innerHTML = display;
 }
-
